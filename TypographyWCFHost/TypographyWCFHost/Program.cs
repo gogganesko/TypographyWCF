7caae7c3433cb930423683ef9a3e3e8246b16562
Georgiy Yurkov <gogganesko@gmail.com>
2020-05-28 21:16:50 +0800

    )))))

---
+++ TypographyWCFHost/TypographyWCFHost/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using Typography_WCF;
+
+namespace TypographyWCFHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost host = new ServiceHost(typeof(Typography_WCF.WorkClasses), new Uri("http://localhost:8733/Design_Time_Addresses/Typography_WCF/Service1/"));
+            host.AddServiceEndpoint(typeof(Typography_WCF.IClasses), new BasicHttpBinding(), "");
+
+
+            host.Open();
+
+            Console.WriteLine("Служба запущенна");
+
+            Console.ReadLine();
+
+            host.Close();
+        }
+    }
+}
---
+++ Typography_WCF/Typography_WCF/IClasses.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+namespace Typography_WCF
+{
+    [ServiceContract]
+    public interface IClasses
+    {
+        [OperationContract]
+        void changePwd(int personID, string password);
+
+        [OperationContract]
+        void checkDB();
+
+        [OperationContract]
+        void addChatMessage(ChatMessage chatMessage);
+        [OperationContract]
+        void delChatMessage(int id);
+        [OperationContract]
+        List<ChatMessage> getChatMessages();
+
+        [OperationContract]
+        void addOrder(Order order);
+
+        [OperationContract]
+        List<Order> getOrdersByDate(int PersonID, DateTime startDate, DateTime endDate);
+
+        [OperationContract]
+        List<Order> getOrders();
+
+        [OperationContract]
+        List<Person> getPersons();
+
+        [OperationContract]
+        void addPerson(Person person);
+
+        [OperationContract]
+        int auth(String login, string password);
+
+        [OperationContract]
+        int logout(int personID);
+
+        [OperationContract]
+        List<PrintType> getPrintTypes();
+
+        [OperationContract]
+        List<Sale> getSales();
+
+        [OperationContract]
+        List<Service> getServices();
+    }
+}
